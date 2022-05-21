5ea822cd47a88924b3606acf9f0b4de11389a7ca
Diego Kaku <diegokaku@gmail.com>
2022-05-21 15:32:53 -0500

    run build

---
+++ client/build/precache-manifest.7eca81c3b97e82899aab639a724128bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94f5666a2de642f0a228c6b375a6096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad5e0758b4e977f91750",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2ec91252ccc38eacabba",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "ad5e0758b4e977f91750",
+    "url": "/static/js/2.3c1aa52f.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.3c1aa52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec91252ccc38eacabba",
+    "url": "/static/js/main.e13ba718.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
